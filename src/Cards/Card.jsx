28a5542c5471ffb09de3eaeb0f90cfb8ff7e1c98
Sardonic1999 <sardorjurayev578@gmail.com>
2021-09-29 11:14:25 +0500

    Cards

--- src/Cards/Card.jsx
+++ src/Cards/Card.jsx
@@ -1,8 +1,69 @@
 import React from "react";
-import { Wrapper } from "./Card_Style";
+import { CardPixel, Pixel, Wrapper, WrapPixel } from "./Card_Style";
+import lamp from "../assets/icons/lamp.svg";
+import blog from "../assets/icons/blog.svg";
+import social from "../assets/icons/social.svg";
+import setting from "../assets/icons/setting.svg";
+import point from "../assets/icons/point.svg";
 
 const Card = () => {
-  return <Wrapper></Wrapper>;
+  return (
+    <Wrapper>
+      <WrapPixel>
+        <Pixel>
+          <h4>Your Vision, Our Future.</h4>
+          <h1>
+            Committed to People, <br /> Committed{" "}
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit.{" "}
+          </p>
+        </Pixel>
+        <CardPixel blue white>
+          <img src={lamp} alt="" />
+          <h1>Creative Ideas</h1>
+          <p>
+            Lorem ipsum dolor sit amet, <br /> consectetur adipiscing elit. Sed{" "}
+            <br /> donec gravida{" "}
+          </p>
+        </CardPixel>
+        <CardPixel>
+          <img src={blog} alt="" />
+          <h1>Beautiful Blog</h1>
+          <p>
+            Lorem ipsum dolor sit amet, <br /> consectetur adipiscing elit. Sed{" "}
+            <br /> donec gravida{" "}
+          </p>
+        </CardPixel>
+      </WrapPixel>
+      <WrapPixel>
+        <CardPixel>
+          <img src={social} alt="" />
+          <h1>User Friendly</h1>
+          <p>
+            Lorem ipsum dolor sit amet, <br /> consectetur adipiscing elit. Sed{" "}
+            <br /> donec gravida{" "}
+          </p>
+        </CardPixel>
+        <CardPixel>
+          <img src={setting} alt="" />
+          <h1>Perfect Showcase</h1>
+          <p>
+            Lorem ipsum dolor sit amet, <br /> consectetur adipiscing elit. Sed{" "}
+            <br /> donec gravida{" "}
+          </p>
+        </CardPixel>
+        <CardPixel>
+          <img src={point} alt="" />
+          <h1>Easy to Use</h1>
+          <p>
+            Lorem ipsum dolor sit amet, <br /> consectetur adipiscing elit. Sed{" "}
+            <br /> donec gravida{" "}
+          </p>
+        </CardPixel>
+      </WrapPixel>
+    </Wrapper>
+  );
 };
 
 export default Card;
--- src/Cards/Card_Style.js
+++ src/Cards/Card_Style.js
@@ -2,15 +2,88 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
   display: flex;
+  flex-direction: column;
   width: 1600px;
-  height: 737.67px;
+  height: fit-content;
   box-sizing: border-box;
-  background: #fff9f5; ;
+  background: #fff9f5;
+  padding: 100px 0px;
+`;
+
+export const WrapPixel = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const Pixel = styled.div`
+  width: 388px;
+  height: 265px;
+  /* background: #4252cf; */
+  background: transparent;
+  border-radius: 10px;
+  box-sizing: border-box;
+  padding: 30px;
+  h4 {
+    font-family: Inter;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 20px;
+    line-height: 160%;
+    letter-spacing: 0.05em;
+    text-transform: uppercase;
+    color: #ff5a05;
+    margin: 0px;
+  }
+  h1 {
+    font-family: Inter;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 30px;
+    line-height: 130%;
+    color: #05000b;
+    margin: 0px;
+  }
+  p {
+    font-family: Inter;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 160%;
+    color: #05000b;
+    opacity: 0.7;
+    margin: 0px;
+  }
+`;
+
+export const CardPixel = styled.div`
   width: 358px;
   height: 265px;
-  background: #4252cf;
+  background: ${({ blue }) => (blue ? "#4252cf" : "#FFFFFF")};
   border-radius: 10px;
+  border: 2px solid rgba(66, 82, 207, 0.1);
+  box-sizing: border-box;
+  border-radius: 10px;
+  margin: 30px;
+  cursor: pointer;
+
+  padding: 30px;
+  h1 {
+    font-family: Inter;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 20px;
+    line-height: 160%;
+    color: ${({ white }) => (white ? "#ffffff" : "black")};
+  }
+  p {
+    font-family: Inter;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 160%;
+    color: ${({ white }) => (white ? "#ffffff" : "black")};
+
+    opacity: 0.8;
+  }
 `;
